f56d6ca1df3e5e79912ee87bf06916baed26bb59
SathyaKrishna-M <maragani.sathyakrishna@klh.edu.in>
2026-06-17 17:07:12 +0530

    gitpages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // process.env.VERCEL is true when deployed on Vercel
-  // Otherwise, fallback to '/SupplyChain/' for GitHub Pages
-  base: process.env.VERCEL ? '/' : '/SupplyChain/',
+  base: process.env.VERCEL ? '/' : '/Intelligent-SupplyChain-System/',
 })
